4f36aa9f96adaf8827d7164ecf0c6908e855dc20
Adeola Adeyemo <adeola.adeyemoj@yahoo.com>
2023-08-15 17:03:26 +0100

    update: add font and connect chakra ui to website
    Signed-off-by: Adeola Adeyemo <adeola.adeyemoj@yahoo.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { ChakraProvider } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
+import { AnimatePresence } from "framer-motion";
+import Head from "next/head";
+
+import { theme } from "../styles/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<meta
+					name="description"
+					content="We partner with leading tech companies & brands 
+					to host brunches, co-living houses at conferences and concerts, 
+					pitch competitions, day parties, kickbacks, and more to bring driven 
+					Black people in tech together in meaningful ways that catalyse relationship-building 
+					among one another."
+				/>
+				<meta
+					name="viewport"
+					content="width=device-width, initial-scale=1"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<ChakraProvider theme={theme}>
+				<AnimatePresence mode="wait">
+					<Component {...pageProps} />
+				</AnimatePresence>
+			</ChakraProvider>
+		</>
+	);
 }
